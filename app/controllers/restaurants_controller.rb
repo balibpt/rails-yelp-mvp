6f79bc6b4d23f939b4ad1a760f323c9145dc3a52
Bali Tok <bali.tok@hotmail.com>
2023-03-08 14:28:59 +0800

    Added show route to display specific restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,17 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %w[show]
+
   def index
     @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
